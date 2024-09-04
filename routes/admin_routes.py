513d75128804383b1b90d7908b2a22a1395148d8
Vitor Miller <vitormillerdetoledo@gmail.com>
2024-09-04 16:38:43 -0300

    Alteração e obtenção de um Produto

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter
+from fastapi import Path
+from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 
+from dtos.alterar_produto_dto import AlterarProdutoDTO
 from dtos.novo_produto_dto import InserirProdutoDTO
-from dtos.excluir_produto_dto import ExcluirProdutoDTO
+from dtos.id_produto_dto import IdProdutoDTO
 from dtos.problem_detail_dto import ProblemDetailsDto
 from models.produto_model import Produto
 from repositories.produto_repo import ProdutoRepo
@@ -14,14 +16,30 @@ async def obter_produtos():
     produtos = ProdutoRepo.obter_todos()
     return produtos
 
-@router.post("/inserir_produto")
+@router.post("/inserir_produto", status_code=201)
 async def inserir_produtos(inputDto:InserirProdutoDTO) -> Produto:
     novo_produto = Produto(None, inputDto.nome, inputDto.preco, inputDto.descricao, inputDto.estoque)
     novo_produto = ProdutoRepo.inserir(novo_produto)
     return novo_produto
 
-@router.post("/excluir_produto")
-async def excluir_produtos(inputDto:ExcluirProdutoDTO) :
+@router.post("/excluir_produto", status_code=204)
+async def excluir_produtos(inputDto:IdProdutoDTO) :
     if(ProdutoRepo.excluir(inputDto.id_produto)): return None
-    pb = ProblemDetailsDto("int",f"O produto com id {inputDto.id_produto} não foi encontrado.", "value_not_found", ["body", "id_produto"])
+    pb = ProblemDetailsDto("int",f"O produto com id <b>{inputDto.id_produto}</b> não foi encontrado.", "value_not_found", ["body", "id_produto"])
+    return JSONResponse(pb.to_dict(),status_code=404) 
+
+
+@router.get("/obter_produto/{id_produto}")
+async def obter_produto(id_produto: int = Path(..., title = "Id do Produto", ge=1) ):
+    produto = ProdutoRepo.obter_um(id_produto)
+    if(produto): return produto
+    pb = ProblemDetailsDto("int",f"O produto com id <b>{id_produto}</b> não foi encontrado.", "value_not_found", ["body", "id_produto"])
+    return JSONResponse(pb.to_dict(),status_code=404) 
+
+
+@router.post("/alterar_produto", status_code=204)
+async def alterar_produtos(inputDto:AlterarProdutoDTO) :
+    produto = Produto(inputDto.id, inputDto.nome,inputDto.preco, inputDto.descricao, inputDto.estoque )
+    if(ProdutoRepo.alterar(produto)): return None
+    pb = ProblemDetailsDto("int",f"O produto com id <b>{inputDto.id}</b> não foi encontrado.", "value_not_found", ["body", "id_produto"])
     return JSONResponse(pb.to_dict(),status_code=404) 
