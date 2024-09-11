adbc9f747b0e33322b783061545dacf9ff83079c
Vitor Miller <vitormillerdetoledo@gmail.com>
2024-09-11 17:02:20 -0300

    Autorização via JWT Corrigida

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,10 +1,8 @@
 from fastapi import Path
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-
 from dtos.alterar_pedido_dto import AlterarPedidoDTO
 from dtos.alterar_produto_dto import AlterarProdutoDTO
-from dtos.entrar_dto import EntrarDTO
 from dtos.novo_produto_dto import InserirProdutoDTO
 from dtos.id_produto_dto import IdProdutoDTO
 from dtos.problem_detail_dto import ProblemDetailsDto
@@ -12,10 +10,10 @@ from models.pedido_model import EstadoPedido
 from models.produto_model import Produto
 from repositories.pedido_repo import PedidoRepo
 from repositories.produto_repo import ProdutoRepo
-from repositories.usuario_repo import UsuarioRepo
+
 from util.auth_jwt import conferir_senha, criar_token
 
-router = APIRouter(prefix="/manager")
+router = APIRouter(prefix="/admin")
 
 @router.get("/obter_produtos")
 async def obter_produtos():
@@ -72,17 +70,3 @@ async def obter_pedido(id_pedido: int = Path(..., title="Id do Pedido", ge=1)):
 async def obter_pedidos_por_estado(estado:EstadoPedido = Path(..., title= "Estado do Pedido")):
     pedidos = PedidoRepo.obter_todos_por_estado(estado.value)
     return pedidos 
-
-
-@router.post("/entrar")
-async def entrar(entrar_dto:EntrarDTO):
-    cliente_entrou = UsuarioRepo.obter_por_email(entrar_dto.email)
-    if (
-        (not cliente_entrou)
-        or (not cliente_entrou.senha)
-        or (not conferir_senha(entrar_dto.senha, cliente_entrou.senha))
-    ):
-       pd = ProblemDetailsDto("str", f"Credenciais Inválidas. Certifique-se de que está cadastrado e de que sua senha está correta", "value_not_found", ["body", "email", "senha"])
-       return JSONResponse(pd.to_dict(),status_code=404) 
-    token = criar_token(cliente_entrou.id, cliente_entrou.nome, cliente_entrou.email, cliente_entrou.perfil)
-    return JSONResponse({"token": token})
